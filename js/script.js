5b57992742708288e953916a7869c65d96354cea
Egor <68065928+E-g-oR@users.noreply.github.com>
2020-11-16 16:38:19 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -215,14 +215,19 @@ const TableContain = document.querySelector('.table__contain'),
   ];
 
 const createTemplate = (item, number) => {
+  console.log(item);
   if (item.name !== null) {
     return `
       <div class="class">
-        <h2 class="class__name"><span class="number">${number}.</span>${item.name}</h2>
-        <p class="class__time">${item.time}</p>
-        <p class="class__kind">${item.kind}</p>
-        <p class="class__room">${item.room}</p>
-        <p class="class__teacher">${item.teacher}</p>
+        <div class="class__info">
+          <h2 class="class__name"><span class="number">${number}.</span>${item.name}</h2>
+          <p class="class__time">${item.time}</p>
+          <div class="class__kind-wrap">
+            <p class="class__kind">${item.kind}</p>
+            <p class="class__room">${item.room}</p>
+            </div>
+          <p class="class__teacher">${item.teacher}</p>
+        </div>
       </div>
     `
   } else return `
@@ -235,14 +240,29 @@ const createTemplate = (item, number) => {
 
 window.addEventListener('load', () => {
   var date = new Date();
-  var today = date.getDay(),
-    todayText = "Понедельник";
-  if (today == 2) todayText = "Вторник"
-  else if (today == 3) todayText = "Среда"
-  else if (today == 4) todayText = "Четверг"
-  else if (today == 5) todayText = "Пятница"
-  else if (today == 6) todayText = "Суббота"
-  else if (today == 7) todayText = "Воскресенье"
+  var today = date.getDay() - 1,
+    todayText = "";
+  if (today == 0) {
+    todayText = "Понедельник"
+  }
+  else if (today == 1) {
+    todayText = "Вторник"
+  }
+  else if (today == 2) {
+    todayText = "Среда"
+  }
+  else if (today == 3) {
+    todayText = "Четверг"
+  }
+  else if (today == 4) {
+    todayText = "Пятница"
+  }
+  else if (today == 5) {
+    todayText = "Суббота"
+  }
+  else if (today == 6) {
+    todayText = "Воскресенье"
+  }
   TableSubtitle.textContent = todayText;
   // fetch(requestURL)
   //   .then(response => response.json())
@@ -259,12 +279,23 @@ window.addEventListener('load', () => {
   //     }
   //   });
 
+  // console.log(table[today]);
+  // var a = 1;
+  // for (number in table[today]) {
+  //   console.log(number);
+  //   TableContain.innerHTML += createTemplate(number, a);
+  //   a++;
+  // }
+
   table.forEach(element => {
-    var a = 1;
-    for (number in element) {
-      console.log(number);
-      TableContain.innerHTML += createTemplate(element[number], a);
-      a++;
+    console.log(table.indexOf(element));
+    if (table.indexOf(element) == today) {
+      var a = 1;
+      for (number in element) {
+        // console.log(number);
+        TableContain.innerHTML += createTemplate(element[number], a);
+        a++;
+      }
     }
   })
 });
